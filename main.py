36d438111470a0771bebb941747a005e326afd11
Margarita Lavruk <margolav27@yandex.ru>
2023-06-12 13:35:22 +0300

    незначительно

--- main.py
+++ main.py
@@ -14,17 +14,6 @@ def generate_and_save_keys(settings):
      cipher_symmetric_key = encrypt_asymmetric( public_key, symmetric_key)
      save_symmetric_key(cipher_symmetric_key, settings['symmetric_key'])
 
-def generate_keys_and_save(args, settings):
-    if args.generation:
-        generate_and_save_keys(settings)
-        #     symmetric_key = generate_symmetric_key()
-        #     private_key, public_key = generate_asymmetric_keys()
-        #     save_asymmetric_keys(private_key, public_key,
-        #                          settings['secret_key'], settings['public_key'])
-        #     cipher_symmetric_key = encrypt_asymmetric(
-        #         public_key, symmetric_key)
-        #     save_symmetric_key(cipher_symmetric_key, settings['symmetric_key'])
-
 def encrypt_file(settings):
     private_key = load_private_key(settings['secret_key'])
     cipher_key = load_symmetric_key(settings['symmetric_key'])
@@ -32,12 +21,7 @@ def encrypt_file(settings):
     text = read_text(settings['initial_file'])
     cipher_text = encrypt_symmetric(symmetric_key, text)
     write_text(cipher_text, settings['encrypted_file'])
-            # private_key = load_private_key(settings['secret_key'])
-            # cipher_key = load_symmetric_key(settings['symmetric_key'])
-            # symmetric_key = decrypt_asymmetric(private_key, cipher_key)
-            # text = read_text(settings['initial_file'])
-            # cipher_text = encrypt_symmetric(symmetric_key, text)
-            # write_text(cipher_text, settings['encrypted_file'])
+
 def decrypt_file(settings):
     private_key = load_private_key(settings['secret_key'])
     cipher_key = load_symmetric_key(settings['symmetric_key'])
@@ -46,13 +30,7 @@ def decrypt_file(settings):
     text = decrypt_symmetric(symmetric_key, cipher_text)
     write_text(text, settings['decrypted_file'])
 
-            # private_key = load_private_key(settings['secret_key'])
-            # cipher_key = load_symmetric_key(settings['symmetric_key'])
-            # symmetric_key = decrypt_asymmetric(private_key, cipher_key)
-            # cipher_text = read_text(settings['encrypted_file'])
-            # text = decrypt_symmetric(symmetric_key, cipher_text)
-            # write_text(text, settings['decrypted_file'])
-
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-set', '--settings', default=SETTINGS_FILE, type=str, help='Позволяет использовать собственный json-файл с указанием путей'
