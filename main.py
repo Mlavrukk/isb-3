18cde94ed239a95cde0f92209af5b0e2e7381a08
Margarita Lavruk <margolav27@yandex.ru>
2023-06-12 13:50:16 +0300

    добавлены докстринги

--- main.py
+++ main.py
@@ -7,14 +7,38 @@ from system_functions import load_settings, save_asymmetric_keys, save_symmetric
 SETTINGS_FILE = 'settings.json'
 
 def generate_and_save_keys(settings):
-     symmetric_key = generate_symmetric_key()
-     private_key, public_key = generate_asymmetric_keys()
-     save_asymmetric_keys(private_key, public_key,
-     settings['secret_key'], settings['public_key'])
-     cipher_symmetric_key = encrypt_asymmetric( public_key, symmetric_key)
-     save_symmetric_key(cipher_symmetric_key, settings['symmetric_key'])
+    """
+    Generates and saves keys using the provided settings.
+
+    Args:
+        settings: A dictionary containing the settings for key generation.
+            Must contain the following keys:
+                - 'secret_key': The file path to save the private key.
+                - 'public_key': The file path to save the public key.
+                - 'symmetric_key': The file path to save the encrypted symmetric key.
+
+    """
+    symmetric_key = generate_symmetric_key()
+    private_key, public_key = generate_asymmetric_keys()
+    save_asymmetric_keys(private_key, public_key,
+    settings['secret_key'], settings['public_key'])
+    cipher_symmetric_key = encrypt_asymmetric( public_key, symmetric_key)
+    save_symmetric_key(cipher_symmetric_key, settings['symmetric_key'])
 
 def encrypt_file(settings):
+    """
+    Encrypts a file using the provided settings.
+
+    Args:
+        settings: A dictionary containing the settings for file encryption.
+            Must contain the following keys:
+                - 'secret_key': The file path to load the private key.
+                - 'public_key': The file path to load the public key.
+                - 'symmetric_key': The file path to load the encrypted symmetric key.
+                - 'initial_file': The file path to the file to be encrypted.
+                - 'encrypted_file': The file path to save the encrypted file.
+
+    """
     private_key = load_private_key(settings['secret_key'])
     cipher_key = load_symmetric_key(settings['symmetric_key'])
     symmetric_key = decrypt_asymmetric(private_key, cipher_key)
@@ -23,6 +47,18 @@ def encrypt_file(settings):
     write_text(cipher_text, settings['encrypted_file'])
 
 def decrypt_file(settings):
+    """
+    Decrypts a file using the provided settings.
+
+    Args:
+        settings : A dictionary containing the settings for file decryption.
+            Must contain the following keys:
+                - 'secret_key': The file path to load the private key.
+                - 'symmetric_key': The file path to load the encrypted symmetric key.
+                - 'encrypted_file': The file path to the encrypted file.
+                - 'decrypted_file': The file path to save the decrypted file.
+
+    """
     private_key = load_private_key(settings['secret_key'])
     cipher_key = load_symmetric_key(settings['symmetric_key'])
     symmetric_key = decrypt_asymmetric(private_key, cipher_key)
